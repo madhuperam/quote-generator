88abb195415397060113d9f8528e39ed5bf31a70
Madhurima <MAPERAM@MAPERAM-L480.in.oracle.com>
2020-12-02 21:57:05 +0530

    completed random quote generator

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      author: "- Albert Einstein",
+      quote: "A person who never made a mistake never tried anything new."
+    }
+    this.setRandomColor = this.setRandomColor.bind(this);
+    this.getRandomQuote = this.getRandomQuote.bind(this);
+  }
+
+  getRandomQuote(){
+    let rand = Math.floor(Math.random()*1643);
+    let author="";
+    let text ="";
+    fetch("https://type.fit/api/quotes")
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      text += data[rand].text;
+      author += data[rand].author;
+      console.log(author);
+      console.log(text);
+      document.getElementById("text").innerHTML = text;
+      document.getElementById("author").innerHTML = "- "+author;
+    });
+    this.setState(()=>({
+      author: author,
+      quote: text
+    }));
+    console.log(author);
+    console.log(text);
+
+  }
+
+  setRandomColor(){
+    const letters = '0123456789ABCDEF';
+    let color = "#";
+    for(var i=0;i<6;i++){
+      color += letters[Math.floor(Math.random()*16)];
+    }
+    if(color === "#ffffff"){
+      color = "#ff4dc4";
+    }
+    this.getRandomQuote();
+    document.body.style.background = color;
+    var btns = document.getElementsByClassName("buttons");
+    for(var i=0;i<btns.length;i++){
+      btns[i].style.background = color;
+    }
+  }
+  render(){
+    return(
+      <div className="App">
+      <div id="quote-box">
+        <div className="content">
+          <div id="text">
+            A person who never made a mistake never tried anything new.
+          </div>
+          <div id="author">
+            - Albert Einstein
+          </div>
+          <div id="buttons">
+            <button className="btn btn-default buttons posts"><i className="fab fa-twitter"/><a href="#" id="tweet-quote"></a></button>
+            <button className="btn btn-default buttons posts"><i className="fab fa-facebook"/><a href="#" id="tweet-quote"></a></button>
+            <button id="new-quote" className="btn btn-default buttons" onClick={this.setRandomColor}>New quote</button>
+          </div>
+        </div>
+
+      </div>
+      <div className="coder">
+        <strong>Coded by Madhurima Peram</strong>
+      </div>
+      </div>
+    );
+  };
 }
 
 export default App;
